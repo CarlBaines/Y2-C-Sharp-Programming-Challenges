19bfb1734ae5bd1047d05092f61b7c1f127eb643
Carl Baines <carltbaines@hotmail.co.uk>
2023-12-13 17:40:33 +0000

    Add files via upload

---
+++ Input Validation and Exception Handling/Celsius to Fahrenheit Converter/Celsius to Fahrenheit Converter/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Celsius_to_Fahrenheit_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float celcius = 0;
+
+            float fahrenheit = 0;
+
+            
+            try
+            {
+                fahrenheit = float.Parse(textBox1.Text);
+                if (fahrenheit < -100 || fahrenheit > 100) {
+                    MessageBox.Show("Temperature out of range");
+                }
+
+                else
+                {
+                    celcius = (fahrenheit - 32) * 5/9;
+                    MessageBox.Show(fahrenheit + "°F is equal to " + celcius + "°C");
+                }
+               
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Input Validation and Exception Handling/ThemePark/ThemePark/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThemePark
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int numOfTickets = 0;
+
+            double totalCost = 0;
+
+            double ticketPrice = 10.99;
+
+            try
+            {
+
+                numOfTickets = int.Parse(textBox1.Text);
+
+                totalCost = (numOfTickets *  ticketPrice);
+                if (totalCost > 40000)
+                {
+                    MessageBox.Show("Ticket Sales are high!");
+                }
+                else
+                {
+                    MessageBox.Show("Tickets are low!");
+                }
+            }
+
+            catch (Exception ex) { 
+
+                MessageBox.Show("Exception: " + ex.Message);
+            }
+            
+
+        }
+    }
+}
